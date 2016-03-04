e35d31799eefa1a0112b4f17bd7dbb0e678ab50e
IkkiTakahashi <dev-mac-admin@exceed-system.co.jp>
2016-03-04 13:33:54 +0900

    Edit Realm Object File

---
+++ TrialMemoAppUsingSwiftRealm/DateTimeLogRealm.swift
@@ -0,0 +1,20 @@
+//
+//  DateTimeLogRealm.swift
+//  TrialMemoAppUsingSwiftRealm
+//
+//  Created by dev-Mac002 on 2016/03/03.
+//  Copyright © 2016年 IkkiTakahashi. All rights reserved.
+//
+
+import RealmSwift
+
+class DateTimeLog: Object {
+    dynamic var id = 0
+    dynamic var date = NSDate()
+    
+    static var maxId = 0
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
---
+++ TrialMemoAppUsingSwiftRealm/ManagementIdCounterRealm.swift
@@ -0,0 +1,20 @@
+//
+//  ManagementIdCounterRealm.swift
+//  TrialMemoAppUsingSwiftRealm
+//
+//  Created by dev-Mac002 on 2016/03/04.
+//  Copyright © 2016年 IkkiTakahashi. All rights reserved.
+//
+
+import RealmSwift
+
+class ManagementIdCounter: Object {
+    dynamic var objectName = ""
+    dynamic var counter = 0
+    
+    override class func primaryKey() -> String {
+        return "objectName"
+    }
+    
+    
+}
